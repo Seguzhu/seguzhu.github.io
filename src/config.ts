924d9811b7364809273c2d7f38a3462026e25402
Seguzhu <seguzhu780@gmail.com>
2026-09-16 09:06:28 +0800

    deploy: src/config.ts

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '彭行也的小站 | 电吉他学习中 · 日语学习中';
+export const SITE_DESCRIPTION = '彭行也的个人网站：多元兴趣者的学习分享、听读体会，以及一些 AI 制作的小工具。';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
