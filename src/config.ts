783bbdf8f67dd856297887cc2b9ca9df2be0f3f1
Seguzhu <seguzhu780@gmail.com>
2026-09-16 17:10:54 +0800

    sync: src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = '彭行也的小站 | 电吉他学习中 · 日语学习中';
 export const SITE_DESCRIPTION = '彭行也的个人网站：多元兴趣者的学习分享、听读体会，以及一些 AI 制作的小工具。';
-export const GENERATE_SLUG_FROM_TITLE = true
+export const GENERATE_SLUG_FROM_TITLE = false
 export const TRANSITION_API = true
